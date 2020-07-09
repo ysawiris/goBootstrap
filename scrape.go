ab453a2b9c87806c800d8d6397443f8164fce42b
Youssef Sawiris <youssef.sawiris@students.dominican.edu>
2020-07-08 19:21:14 -0700

    add selector to get all components

--- scrape.go
+++ scrape.go
@@ -13,12 +13,19 @@ func main() {
 	c := colly.NewCollector()
 
 	// On every a element which has href attribute call callback
-	c.OnHTML("#bd-docs-nav > div.bd-toc-item.active > ul", func(e *colly.HTMLElement) {
+	c.OnHTML("#bd-docs-nav > div.bd-toc-item.active > ul > li", func(e *colly.HTMLElement) {
+		e.ForEach("a", func(_ int, e *colly.HTMLElement) {
+			link := e.Attr("href")
+			// Print link
+			fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+			//Visit each link
+			c.Visit("https://getbootstrap.com/docs/4.5/components/" + link)
+		})
 
-		link := e.Attr("href")
+		// link := e.Attr("href")
 
-		// Print link
-		fmt.Printf("Link found: %q -> %s\n", e.Text, link)
+		// // Print link
+		// fmt.Printf("Link found: %q -> %s\n", e.Text, link)
 	})
 
 	// Before making a request print "Visiting ..."
@@ -26,6 +33,6 @@ func main() {
 		fmt.Println("Visiting", r.URL.String())
 	})
 
-	// Start scraping on https://hackerspaces.org
+	// Start scraping on https://getboostrap.com
 	c.Visit("https://getbootstrap.com/docs/4.5/components/alerts/")
 }
